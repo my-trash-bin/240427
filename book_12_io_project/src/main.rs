f2498672b1759459009ddfdd54519c504e732036
Juyeong Maing <mjy9088@naver.com>
2024-04-27 01:11:34 +0900

    WIP: add book_12_io_project

---
+++ book_12_io_project/src/main.rs
@@ -0,0 +1,35 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let result: Config = Config { query, file_path };
+
+        result
+    }
+}
